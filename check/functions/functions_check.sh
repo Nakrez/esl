09155621b9fb2bd3f9222209c2f20a0c6929d953
Baptiste Covolato <Baptiste Covolato>
2012-12-30 22:01:42 +0100

    Add some few tests

---
+++ check/functions/functions_check.sh
@@ -0,0 +1,19 @@
+#! /bin/sh
+
+categorie="functions"
+count=4
+curr=1
+win=0
+
+while test $curr -le $count; do
+    file=$categorie$curr".esl"
+    ./esl "./check/$categorie/$file" > ./check/out
+    if diff "./check/out" "./check/$categorie/out$curr" > /dev/null; then
+        win=$(($win+1))
+    else
+        echo "File $file in $categorie failed"
+    fi
+    curr=$(($curr+1));
+done
+
+echo "Categorie $categorie passed $win/$count"
