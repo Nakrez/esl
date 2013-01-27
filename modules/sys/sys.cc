12d82e53af55f22062c75c7fcfc822e2fb5a0018
Baptiste Covolato <Baptiste Covolato>
2013-01-27 13:49:31 +0100

    Add exit function to Sys module

--- modules/sys/sys.cc
+++ modules/sys/sys.cc
@@ -4,11 +4,30 @@
 void Sys::init ()
 {
     register_function("fork", new esl::Delegate<Sys>(this, &Sys::fork_fun));
+    register_function("exit", new esl::Delegate<Sys>(this, &Sys::exit_fun));
 }
 
 esl::MemoryObject<esl::Content>* Sys::fork_fun (const esl::Params&)
 {
     return new esl::MemoryObject<esl::Content> (new esl::IntObject(fork()));
+}
+
+esl::MemoryObject<esl::Content>* Sys::exit_fun (const esl::Params& params)
+{
+    if (params.count() == 0)
+        exit(0);
+
+    esl::IntObject* return_value = nullptr;
+
+    return_value = dynamic_cast<esl::IntObject*>(params.get_params(1)->data_get());
+
+    if (return_value)
+        exit(return_value->data_get());
+    else
+        throw esl::Exception("First parameter of exit must be an integer");
+
+    // Never reached
+    return new esl::MemoryObject<esl::Content> (new esl::IntObject(0));
 }
 
 extern "C"
