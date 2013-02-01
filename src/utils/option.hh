c677f1a99cfff72021e3b48b1d29948c3c722ce3
Nicolas <nsurleraux@gmail.com>
2013-02-01 22:41:17 +0100

    Option.cc following

---
+++ src/utils/option.hh
@@ -0,0 +1,16 @@
+#ifndef OPTION_HH
+#define OPTION_HH
+#include <map>
+#include <string>
+
+class Option
+{
+    public:
+        Option ();
+        ~Option ();
+
+        void ee_optn();
+        void byte_optn();
+        void ast_optn();
+};
+#endif /* OPTION_HH */
