2a13c29e4a431fa39dcefbf6696dff12f77e355d
Baptiste Covolato <b.covolato@gmail.com>
2013-03-11 16:14:53 +0100

    Update defalut visitor for bytecode

--- src/ast/visitor.cc
+++ src/ast/visitor.cc
@@ -13,4 +13,28 @@ namespace ast
     {
         ast.accept(*this);
     }
+
+    void Visitor::operator()(AstList& ast)
+    {
+        for (auto elem : ast.list_get())
+            elem->accept(*this);
+    }
+
+    void Visitor::operator()(InstrList& ast)
+    {
+        for (auto elem : ast.list_get())
+            elem->accept(*this);
+    }
+
+    void Visitor::operator()(DecList& ast)
+    {
+        for (auto elem : ast.list_get())
+            elem->accept(*this);
+    }
+
+    void Visitor::operator()(VarDecList& ast)
+    {
+        for (auto elem : ast.list_get())
+            elem->accept(*this);
+    }
 } // namespace ast
--- src/ast/visitor.hh
+++ src/ast/visitor.hh
@@ -14,8 +14,9 @@ namespace ast
             virtual ~Visitor();
 
             virtual void operator()(Ast&);
-            virtual void operator()(AstList&) = 0;
+            virtual void operator()(AstList&);
 
+            virtual void operator()(DecList&);
             virtual void operator()(IntExp&) = 0;
             virtual void operator()(OpExp&) = 0;
             virtual void operator()(StringExp&) = 0;
@@ -29,7 +30,7 @@ namespace ast
             virtual void operator()(IfInstr&) = 0;
             virtual void operator()(ElseInstr&) = 0;
             virtual void operator()(WhileInstr&) = 0;
-            virtual void operator()(InstrList&) = 0;
+            virtual void operator()(InstrList&);
             virtual void operator()(ImportInstr&) = 0;
 
             virtual void operator()(VarId&) = 0;
@@ -41,10 +42,10 @@ namespace ast
 
             virtual void operator()(FunctionDec&) = 0;
             virtual void operator()(VarDec&) = 0;
+            virtual void operator()(VarDecList&);
             virtual void operator()(MethodDec&) = 0;
             virtual void operator()(AttributDec&) = 0;
             virtual void operator()(ClassDec&) = 0;
-            virtual void operator()(DecList&) = 0;
     };
 } // namespace ast
 
