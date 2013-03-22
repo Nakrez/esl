d990edbb8de95978db66bea8915fd690e41fe90c
Baptiste Covolato <b.covolato@gmail.com>
2013-03-22 11:22:48 +0100

    Add Test for bytecode generation

---
+++ check/bytecode/bytecode_check.sh
@@ -0,0 +1,22 @@
+#! /bin/sh
+
+categorie="bytecode"
+count=1
+curr=1
+win=0
+
+while test $curr -le $count; do
+    file=$categorie$curr".esl"
+    ./esl --byte "./check/$categorie/$file" > ./check/$categorie/out
+    if diff "./check/$categorie/out" "./check/$categorie/out$curr" > /dev/null; then
+        win=$(($win+1))
+    else
+        echo "File $file in $categorie failed"
+        rm -f ./check/$categorie/out
+        exit 1
+    fi
+    curr=$(($curr+1));
+done
+
+rm -f ./check/$categorie/out
+echo "Categorie $categorie passed $win/$count"
