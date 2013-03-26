f7781aa4cd15ed2d00902db6a24ad63d545e9363
Baptiste Covolato <b.covolato@gmail.com>
2013-03-26 18:44:03 +0100

    DefaultVisitor OK

--- src/ast/default-visitor.hxx
+++ src/ast/default-visitor.hxx
@@ -14,99 +14,97 @@ namespace ast
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<AstList>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<AstList>::type& l)
     {
-
+        for (auto elem : l.list_get())
+            this->visit(elem);
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<DecList>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<DecList>::type& l)
     {
-
+        for (auto elem : l.list_get())
+            this->visit(elem);
     }
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<IntExp>::type&)
-    {
-
-    }
+    {}
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<OpExp>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<OpExp>::type& e)
     {
-
+        this->visit(e.lop_get());
+        this->visit(e.rop_get());
     }
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<StringExp>::type&)
-    {
-
-    }
+    {}
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<FunctionCallExp>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<FunctionCallExp>::type& e)
     {
-
+        this->visit(e.args_get());
     }
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<ReturnExp>::type&)
-    {
-
-    }
+    {}
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<BreakExp>::type&)
-    {
-
-    }
+    {}
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<ContinueExp>::type&)
-    {
+    {}
 
+    template <template <typename> class Constness>
+    void
+    GenDefaultVisitor<Constness>::operator()(typename Constness<NewExp>::type& e)
+    {
+        this->visit(e.exp_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<NewExp>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<AssignExp>::type& e)
     {
-
+        this->visit(e.var_get());
+        this->visit(e.exp_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<AssignExp>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<IfInstr>::type& i)
     {
-
+        this->visit(i.condition_get());
+        this->visit(i.exp_true_get());
+        this->visit(i.exp_else_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<IfInstr>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<ElseInstr>::type& i)
     {
-
+        this->visit(i.instr_list_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<ElseInstr>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<WhileInstr>::type& i)
     {
-
-    }
-
-    template <template <typename> class Constness>
-    void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<WhileInstr>::type&)
-    {
+        this->visit(i.instr_list_get());
+        this->visit(i.condition_get());
 
     }
 
@@ -121,63 +119,65 @@ namespace ast
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<ImportInstr>::type&)
-    {
-
-    }
+    {}
 
     template <template <typename> class Constness>
     void
     GenDefaultVisitor<Constness>::operator()(typename Constness<VarId>::type&)
+    {}
+
+    template <template <typename> class Constness>
+    void
+    GenDefaultVisitor<Constness>::operator()(typename Constness<AttributVar>::type& v)
     {
+        v.call_var_get().accept(*this);
+    }
+
+    template <template <typename> class Constness>
+    void
+    GenDefaultVisitor<Constness>::operator()(typename Constness<MethodCallVar>::type& v)
+    {
+        this->visit(v.var_get());
+        this->visit(v.call_get());
 
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<AttributVar>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<ModuleCallVar>::type& v)
     {
-
+        this->visit(v.var_get());
+        this->visit(v.call_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<MethodCallVar>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<ModuleAttributVar>::type& v)
     {
-
+        this->visit(v.var_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<ModuleCallVar>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<ArrayVar>::type& v)
     {
-
+        this->visit(v.var_get());
+        this->visit(v.exp_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<ModuleAttributVar>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<FunctionDec>::type& d)
     {
+        this->visit(d.args_get());
+        this->visit(d.body_get());
 
     }
 
     template <template <typename> class Constness>
-    void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<ArrayVar>::type&)
+    void GenDefaultVisitor<Constness>::operator()(typename Constness<VarDec>::type& v)
     {
-
-    }
-
-    template <template <typename> class Constness>
-    void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<FunctionDec>::type&)
-    {
-
-    }
-
-    template <template <typename> class Constness>
-    void GenDefaultVisitor<Constness>::operator()(typename Constness<VarDec>::type&)
-    {
-
+        this->visit(v.exp_get());
     }
 
     template <template <typename> class Constness>
@@ -190,23 +190,24 @@ namespace ast
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<MethodDec>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<MethodDec>::type& d)
     {
-
+        this->visit(d.args_get());
+        this->visit(d.body_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<AttributDec>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<AttributDec>::type& d)
     {
-
+        this->visit(d.value_get());
     }
 
     template <template <typename> class Constness>
     void
-    GenDefaultVisitor<Constness>::operator()(typename Constness<ClassDec>::type&)
+    GenDefaultVisitor<Constness>::operator()(typename Constness<ClassDec>::type& d)
     {
-
+        this->visit(d.components_get());
     }
 } // namespace ast
 
